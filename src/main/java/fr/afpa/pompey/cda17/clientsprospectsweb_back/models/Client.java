3ed71d0b62f57bac0992b6784ea0fc94e3edd820
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-03-20 14:27:17 +0100

    Implémentation des beans et ajout des annotations de vérification

--- src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/models/Client.java
+++ src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/models/Client.java
@@ -1,4 +1,82 @@
 package fr.afpa.pompey.cda17.clientsprospectsweb_back.models;
 
-public class Client {
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Classe héritant de Societe représentant une société cliente
+ */
+public class Client extends Societe {
+
+
+    @NotNull
+    @Min(201)
+    private long chiffreDAffaire;
+
+    @NotNull
+    @Min(1)
+    private int nombreEmployes;
+
+
+    /**
+     * Constructeur d'insertion ; instancie un client à insérer dans la base de données n'ayant donc pas encore
+     * d'identifiant
+     *
+     * @param raisonSociale   String
+     * @param telephone       String
+     * @param adresse         Adresse
+     * @param mail            String
+     * @param commentaire     String
+     * @param chiffreDAffaire long
+     * @param nombreEmployes  int
+     */
+    public Client(String raisonSociale, String telephone, Adresse adresse, String mail, String commentaire,
+                  long chiffreDAffaire, int nombreEmployes) {
+        super(raisonSociale, telephone, adresse, mail, commentaire);
+        setChiffreDAffaire(chiffreDAffaire);
+        setNombreEmployes(nombreEmployes);
+    }
+
+    /**
+     * Constructeur de chargement ; instancie un client chargé depuis la base de données, incluant son identifiant
+     *
+     * @param identifiant     int
+     * @param raisonSociale   String
+     * @param telephone       String
+     * @param adresse         Adresse
+     * @param mail            String
+     * @param commentaire     String
+     * @param chiffreDAffaire long
+     * @param nombreEmployes  int
+     */
+    public Client(int identifiant, String raisonSociale, String telephone, Adresse adresse, String mail,
+                  String commentaire, long chiffreDAffaire, int nombreEmployes) {
+        super(identifiant, raisonSociale, telephone, adresse, mail, commentaire);
+        setChiffreDAffaire(chiffreDAffaire);
+        setNombreEmployes(nombreEmployes);
+    }
+
+    /**
+     * Constructeur implicite
+     */
+    public Client() {
+        super();
+    }
+
+    public long getChiffreDAffaire() {
+        return chiffreDAffaire;
+    }
+
+    public void setChiffreDAffaire(long chiffreDAffaire) {
+        this.chiffreDAffaire = chiffreDAffaire;
+    }
+
+    public int getNombreEmployes() {
+        return nombreEmployes;
+    }
+
+    public void setNombreEmployes(int nombreEmployes) {
+        this.nombreEmployes = nombreEmployes;
+    }
+
 }
--- src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/models/Societe.java
+++ src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/models/Societe.java
@@ -1,4 +1,136 @@
 package fr.afpa.pompey.cda17.clientsprospectsweb_back.models;
 
-public class Societe {
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
+
+/**
+ * Classe abstraite représentant une société
+ */
+public abstract class Societe {
+
+
+    @NotNull
+    @Positive
+    private Integer identifiant;
+
+    @NotBlank
+    private String raisonSociale;
+
+    @Pattern(regexp = "^(0|([+]|00)[0-9]{2,3})[0-9]{9}$")
+    private String telephone;
+
+    @Valid
+    private Adresse adresse;
+
+    @Pattern(regexp = "^([^ @.-]([.-][^ @.-])?)+@([^ @.-]([.-][^ @.-])?)+[.][a-zA-Z]+$")
+    private String mail;
+
+    @NotNull
+    private String commentaire;
+
+
+    /**
+     * Constructeur d'insertion ; instancie une société à insérer dans la base de données n'ayant donc pas encore
+     * d'identifiant
+     *
+     * @param raisonSociale String
+     * @param telephone     String
+     * @param adresse       Adresse
+     * @param mail          String
+     * @param commentaire   String
+     */
+    public Societe(String raisonSociale, String telephone, Adresse adresse, String mail, String commentaire) {
+        // l'identifiant est initialisé dans le contructeur de la classe fille
+        setRaisonSociale(raisonSociale);
+        setTelephone(telephone);
+        setAdresse(adresse);
+        setMail(mail);
+        setCommentaire(commentaire);
+    }
+
+    /**
+     * Constructeur de chargement ; instancie une société chargée depuis la base de données, incluant son identifiant
+     *
+     * @param identifiant   int
+     * @param raisonSociale String
+     * @param telephone     String
+     * @param adresse       Adresse
+     * @param mail          String
+     * @param commentaire   String
+     */
+    public Societe(int identifiant, String raisonSociale, String telephone, Adresse adresse, String mail,
+                   String commentaire) {
+        setIdentifiant(identifiant);
+        setRaisonSociale(raisonSociale);
+        setTelephone(telephone);
+        setAdresse(adresse);
+        setMail(mail);
+        setCommentaire(commentaire);
+    }
+
+    /**
+     * Constructeur implicite
+     */
+    public Societe() {
+        // l'identifiant est initialisé dans le contructeur de la classe fille
+    }
+
+
+    public Integer getIdentifiant() {
+        return identifiant;
+    }
+
+    protected void setIdentifiant(int identifiant) {
+        this.identifiant = identifiant;
+    }
+
+    public String getRaisonSociale() {
+        return raisonSociale;
+    }
+
+    public void setRaisonSociale(String raisonSociale) {
+        this.raisonSociale = raisonSociale;
+    }
+
+    public String getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(String telephone) {
+        this.telephone = telephone.trim();
+    }
+
+    public Adresse getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse(Adresse adresse) {
+        this.adresse = adresse;
+    }
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+    public String getCommentaire() {
+        return commentaire;
+    }
+
+    public void setCommentaire(String commentaire) {
+        this.commentaire = commentaire;
+    }
+
+
+    @Override
+    public String toString() {
+        // On ne retourne ici que la raison sociale pour avoir un affichage propre dans une comboBox
+        return this.raisonSociale;
+    }
 }
