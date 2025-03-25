5a7954bd8a9c4c304774b871da234e72aa66a99d
BenjaminPlumejeau-afpa <benjamin.plumejeau@afpa-dev-pompey.fr>
2025-03-25 15:55:37 +0100

    Ajout de la connexion à la base de données MySql

--- src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/frontControler/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Package relatif au routeur des pages de l'application.
- */
-package fr.afpa.pompey.cda17.clientsprospectsweb_back.frontControler;
--- src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/servlets/FrontController.java
+++ src/main/java/fr/afpa/pompey/cda17/clientsprospectsweb_back/servlets/FrontController.java
@@ -1,6 +1,8 @@
-package fr.afpa.pompey.cda17.clientsprospectsweb_back.frontControler;
+package fr.afpa.pompey.cda17.clientsprospectsweb_back.servlets;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.logging.Logger;
@@ -8,11 +10,14 @@ import java.util.logging.Logger;
 import fr.afpa.pompey.cda17.clientsprospectsweb_back.controllers.ICommand;
 import fr.afpa.pompey.cda17.clientsprospectsweb_back.controllers.PageAccueilController;
 import fr.afpa.pompey.cda17.clientsprospectsweb_back.controllers.clients.*;
+import jakarta.annotation.Resource;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
 
 /**
  * Routeur pour les pages de l'application.
@@ -21,6 +26,11 @@ import jakarta.servlet.http.HttpServletResponse;
 public class FrontController extends HttpServlet {
     private Map commands = new HashMap();
     private final Logger LOGGER = Logger.getLogger(FrontController.class.getName());
+
+    @Resource(name = "jdbc/mysql")
+    public static DataSource dataSource;
+    public Connection connection;
+
 
     /**
      * Initialise la collection des commandes de routage.
@@ -35,6 +45,17 @@ public class FrontController extends HttpServlet {
         commands.put("modifierClient", new ModificationClientController());
         commands.put("supprimerClient", new SuppressionClientController());
         commands.put("submitAjouterClient", new AjoutClientController());
+
+        // Ouverture de la connexion à la base de données
+        try {
+            connection = dataSource.getConnection();
+            LOGGER.info("\n    ==== CONNECTION OK ====");
+        } catch (SQLException sqle) {
+            //TODO Gestion de l'exception - ServletException ?
+//            throw new ServletException("Problème d'ouverture de connection à la base de données", sqle);
+            LOGGER.warning("\n    SQLException : " + sqle.getMessage());
+            LOGGER.info("\n    ==== CONNECTION ECHEC ====");
+        }
     }
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) {
@@ -46,6 +67,15 @@ public class FrontController extends HttpServlet {
     }
 
     public void destroy() {
+        try {
+            connection.close();
+            LOGGER.info("\n    ==== FERMETURE OK ====");
+        } catch (SQLException sqle) {
+            //TODO Gestion de l'exception - ServletException ?
+//            throw new ServletException("Problème de fermeture de connection à la base de données", sqle);
+            LOGGER.warning("\n    SQLException : " + sqle.getMessage());
+            LOGGER.info("\n    ==== FERMETURE ECHEC ====");
+        }
     }
 
 
